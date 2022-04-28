3ce2b0d93c7c19cc314c731a2c6f737fd6fd54fa
Ductm <ductm@fireapps.vn>
2022-04-28 15:02:52 +0700

    feat: add gh page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
+    <h1>Duc Tran</h1>
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
   </div>
 </template>
