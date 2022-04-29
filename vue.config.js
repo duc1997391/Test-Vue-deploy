d8f145beb226d14a91c0dc053902b681373c8815
Ductm <ductm@fireapps.vn>
2022-04-29 17:04:04 +0700

    feat: add deploy githup/s3

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,8 @@
 const { defineConfig } = require("@vue/cli-service");
+
+console.log(process.env.NODE_ENV);
+
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === "production" ? "/Test-Vue-deploy/" : "/",
+  publicPath: process.env.NODE_ENV === "github" ? "/Test-Vue-deploy/" : "/",
 });
